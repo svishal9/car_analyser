ec84da95595a4e31b0b70ce8efcc0cb587704044
svishal9 <shivendra.vishal@gmail.com>
2026-02-11 23:02:03 +1100

    using poetry for package management only

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "${script_directory}/.."
 
-poetry install
+poetry install --no-root
 
 echo "Fetched all dependencies" >&2
